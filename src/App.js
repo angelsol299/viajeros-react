0af929aebc89dc58bc5b9b97f0feec9e1d8f4bd9
angelsol1599 <angel.osoria@sveasolar.com>
2019-09-28 23:37:19 +0200

    articulos page first elements

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './container/Home';
 import Cards from './container/Cards';
-import Articles from './container/Articles';
+import ArticlesMain from './container/articles/ArticlesMain';
 
 const Hello = () => {
   return (
@@ -18,7 +18,7 @@ const App = () => {
       <BrowserRouter>
         <Route exact path="/" component={Home} />
         <Route path="/posts" component={Cards} />
-        <Route path="/articles" component={Articles} />
+        <Route path="/articles" component={ArticlesMain} />
         <Route path="/hello" component={Hello} />
       </BrowserRouter>
     </div>
